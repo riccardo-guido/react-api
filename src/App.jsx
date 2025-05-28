c509aa4ca79502c65cf19b58f12034713c5c0580
riccardo-guido <guidoriccardo02@gmail.com>
2025-05-28 16:17:28 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Main from "./components/layout/Main";
+
+export default function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
---
+++ src/components/layout/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main>
+      <h1>Prova</h1>
+    </main>
+  );
+}
