edc6d625fb919b59d9bab1f4bcb0268aaea8b853
riccardo-guido <guidoriccardo02@gmail.com>
2025-05-28 16:42:17 +0200

    printed-list-from-api-to-console

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,7 +1,14 @@
+import axios from "axios";
+
+const apiBaseUrl = "https://lanciweb.github.io/demo/api/actors";
+
 export default function Main() {
+  axios.get(`${apiBaseUrl}`).then((res) => {
+    console.log(res.data);
+  });
   return (
     <main>
-      <h1>Prova</h1>
+      <h1>React Api</h1>
     </main>
   );
 }
